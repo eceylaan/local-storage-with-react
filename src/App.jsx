deb722dad0f2ea7f15f92c4810488fff16efae97
eceylaan <ceylann.ece@gmail.com>
2024-08-27 20:34:26 +0300

    local-storage-with-react eklendi

---
+++ src/App.jsx
@@ -0,0 +1,156 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [isOpen, setOpen] = useState(false);
+
+  useEffect(() => {
+    setData(JSON.parse(localStorage.data));
+  }, []);
+
+  function save() {
+    localStorage.data = JSON.stringify(data);
+  }
+
+  function updateRecord(record) {
+    let foundRecord = data.find((x) => x.id === record.id);
+    // referansı bozmamak için object assign kullanıyoruz
+    // eğer referansı kırarsak bu sefer gösterim sırası bozulur
+    // eğer bu notları çözemezseniz referansı kırıp arayüzde probleme odaklanın
+    Object.assign(foundRecord, record);
+    setData([...data]);
+    save();
+  }
+
+  function deleteRecord(id) {
+    if (!confirm("Emin misiniz?")) {
+      return;
+    }
+
+    setData(data.filter((x) => x.id !== id));
+    save();
+  }
+
+  let id = 6;
+  function generateId() {
+    return id++;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formObj = Object.fromEntries(formData);
+    formObj.id = generateId();
+    setData([...data, formObj]);
+    setOpen(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Öğrenci bilgi sistemi <button onClick={() => setOpen(true)}>yeni</button>
+      </h1>
+      {isOpen && (
+        <div className="dialog-overlay">
+          <dialog open={isOpen}>
+            <form onSubmit={handleSubmit}>
+              <div className="studentTableCol">
+                <input type="text" required name="ad" placeholder="ad" />
+              </div>
+              <div className="studentTableCol">
+                <input type="text" required name="soyad" placeholder="soyad" />
+              </div>
+              <div className="studentTableCol">
+                <input type="email" required name="ePosta" placeholder="e-posta" />
+              </div>
+              <div className="studentTableCol">
+                <input type="date" required name="dogumTarihi" />
+              </div>
+              <div className="studentTableCol">
+                <button onClick={() => setOpen(false)} type="button">
+                  vazgeç
+                </button>
+                <button className="saveBtn" type="submit">
+                  ekle
+                </button>
+              </div>
+            </form>
+          </dialog>
+        </div>
+      )}
+
+      <div className="studentTable">
+        <ul className="studentTableTitles">
+          <li>Ad</li>
+          <li>Soyad</li>
+          <li>E-Posta Adresi</li>
+          <li>Doğum Tarihi</li>
+          <li>#</li>
+        </ul>
+        {data.map((x) => (
+          <StudentRow key={x.id} {...x} deleteRecord={deleteRecord} updateRecord={updateRecord} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function StudentRow({ id, ad, soyad, ePosta, dogumTarihi, updateRecord, deleteRecord }) {
+  const [isEditing, setEditing] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formObj = Object.fromEntries(formData);
+    formObj.id = id;
+    updateRecord(formObj);
+    setEditing(false);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} onDoubleClick={() => setEditing(true)}>
+      {isEditing ? (
+        <>
+          <div className="studentTableCol">
+            <input type="text" required name="ad" defaultValue={ad} />
+          </div>
+          <div className="studentTableCol">
+            <input type="text" required name="soyad" defaultValue={soyad} />
+          </div>
+          <div className="studentTableCol">
+            <input type="email" required name="ePosta" defaultValue={ePosta} />
+          </div>
+          <div className="studentTableCol">
+            <input type="date" required name="dogumTarihi" defaultValue={dogumTarihi} />
+          </div>
+          <div className="studentTableCol">
+            <button type="button" onClick={() => setEditing(false)}>
+              vazgeç
+            </button>
+            <button className="saveBtn" type="submit">
+              kaydet
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="studentTableCol">{ad}</div>
+          <div className="studentTableCol">{soyad}</div>
+          <div className="studentTableCol">{ePosta}</div>
+          <div className="studentTableCol">{dogumTarihi.split("-").reverse().join(".")}</div>
+          <div className="studentTableCol">
+            <button type="button" onClick={() => setEditing(true)}>
+              düzenle
+            </button>
+            <button className="delBtn" type="button" onClick={() => deleteRecord(id)}>
+              sil
+            </button>
+          </div>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default App;
